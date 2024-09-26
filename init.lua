6834aac6bf67460c2c2c47ee7368c352467a2b4c
Ricardo Hortelano <ricardo.hortelano@gmail.com>
2024-09-26 15:18:22 +0200

    Add missing lualine start line

--- init.lua
+++ init.lua
@@ -197,6 +197,8 @@ require('lspconfig').ruff.setup {
   cmd_env = { RUFF_TRACE = "messages" }
 }
 
+require('lualine').setup()
+
 local ok, _ = pcall(vim.cmd, 'colorscheme catppuccin-mocha')
 if not ok then
   vim.cmd 'colorscheme default' -- if the above fails, then use default
