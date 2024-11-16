0b9fe85f5b85def649fdfb3a5c13c09458c2c0a4
ricardo <ricardo@ric-desktop.home>
2024-11-16 23:38:53 +0100

    add i3 and i3blocks config

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -8,3 +8,8 @@ mkdir -p ~/.config/nvim/
 ln -sf $PWD/initrc.vim ~/.config/nvim/initrc.vim
 ln -sf $PWD/init.lua ~/.config/nvim/init.lua
 
+mkdir -p ~/.config/i3/
+ln -sf $PWD/.config/i3/config ~/.config/i3/config
+
+mkdir -p ~/.config/i3blocks/
+ln -sf $PWD/.config/i3blocks/config ~/.config/i3blocks/config
